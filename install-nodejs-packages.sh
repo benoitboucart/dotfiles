5cc11da3932a15ec4162b2aa01ff913ff4aa9e24
Benoît Boucart <benoit@webbb.be>
2017-11-27 11:51:12 +0100

    Node packages update

--- install-nodejs-packages.sh
+++ install-nodejs-packages.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # Node packages
-npm install -g gitmoji-cli
-npm install -g ember-cli
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g bower
 npm install -g webpack
 npm install -g yarn
-npm install -g gtop
