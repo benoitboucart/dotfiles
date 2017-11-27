eb9f88debd10ed2cc70470adeb1afe6063fa8d00
Benoît Boucart <benoit@webbb.be>
2017-11-27 11:50:44 +0100

    Brew store apps

--- install-brew-store-apps.sh
+++ install-brew-store-apps.sh
@@ -7,24 +7,11 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install Mac App Store apps
-mas install 443987910 # 1Password
-mas install 425424353 # The Unarchiver
-mas install 1020812363 # CopyClip 2
-mas install 411643860 # DaisyDisk
-mas install 1054607607 # Helium
-mas install 1014850245 # Monit
 mas install 409789998 # Twitter
 mas install 803453959 # Slack
-mas install 457622435 # Yoink
-mas install 507257563 # Sip
-mas install 880001334 # Reeder
-mas install 1153157709 # Speedtest
-mas install 409183694 # Keynote
-mas install 409201541 # Pages
-mas install 409203825 # Numbers
 mas install 497799835 # Xcode
-mas install 1141688067 # Coin Tick
-mas install 568494494 # Pocket
+mas install 585829637 # Todoist
+mas install 918207447 # Annotate
 
 # Cleanup
 brew cleanup
