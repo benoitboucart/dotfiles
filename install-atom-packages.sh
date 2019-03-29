55c270a8bbf8f1a8b4a756d44f021dcecf2b6419
Benoît Boucart <benoitboucart@c02xkblkjg5j.hogeschool-wvl.be>
2019-03-29 15:05:58 +0100

    No atom

--- install-atom-packages.sh
+++ install-atom-packages.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 # Atom packages
-apm install atom-autocomplete-php
-apm install emmet
-apm install language-blade
-apm install language-groovy
-apm install language-vue
-apm install lines
-apm install linter
-apm install linter-php
-apm install minimap
-apm install pigments
-apm install wakatime
-apm install language-docker
-apm install atom-typescript
-apm install language-api-blueprint
-apm install atom-beautify
-apm install color-picker
-apm install php-twig
-apm install language-babel
-apm install atom-live-server
-apm install csslint
-apm install linter
-apm install linter-ui-default
-apm install linter-stylelint
-apm install platform-io-ide-terminal
+# apm install atom-autocomplete-php
+# apm install emmet
+# apm install language-blade
+# apm install language-groovy
+# apm install language-vue
+# apm install lines
+# apm install linter
+# apm install linter-php
+# apm install minimap
+# apm install pigments
+# apm install wakatime
+# apm install language-docker
+# apm install atom-typescript
+# apm install language-api-blueprint
+# apm install atom-beautify
+# apm install color-picker
+# apm install php-twig
+# apm install language-babel
+# apm install atom-live-server
+# apm install csslint
+# apm install linter
+# apm install linter-ui-default
+# apm install linter-stylelint
+# apm install platform-io-ide-terminal
