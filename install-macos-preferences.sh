793409121ddf15f7395227c3055c710cfa287496
Benoît Boucart <msnland@gmail.com>
2019-03-30 10:55:32 +0100

    MacOS pref

--- install-macos-preferences.sh
+++ install-macos-preferences.sh
@@ -69,11 +69,11 @@ defaults write com.apple.menuextra.battery ShowPercent -bool true
 # defaults write com.apple.finder QuitMenuItem -bool true
 #
 # # Finder: show status bar
-# defaults write com.apple.finder ShowStatusBar -bool true
-#
+defaults write com.apple.finder ShowStatusBar -bool true
+
 # # Finder: show path bar
-# defaults write com.apple.finder ShowPathbar -bool true
-#
+defaults write com.apple.finder ShowPathbar -bool true
+
 # # Automatically hide and show the Dock
 # defaults write com.apple.dock autohide -bool true
 #
@@ -119,38 +119,38 @@ defaults write com.apple.menuextra.battery ShowPercent -bool true
 # defaults write NSGlobalDomain InitialKeyRepeat -int 15
 #
 # # Require password immediately after sleep or screen saver begins
-# defaults write com.apple.screensaver askForPassword -int 1
-# defaults write com.apple.screensaver askForPasswordDelay -int 5
-#
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 5
+
 # # Save screenshots to the desktop
-# defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
-#
+defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
+
 # # Disable shadow in screenshots
 # defaults write com.apple.screencapture disable-shadow -bool true
 #
 # # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 # defaults write com.apple.screencapture type -string "jpg"
 #
-# # Avoid creating .DS_Store files on network or USB volumes
-# defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-# defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-#
-# # Disable the warning when changing a file extension
-# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-#
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
 # # Enable Dashboard
 # defaults write com.apple.dashboard mcx-disabled -bool false
 #
 # # Show Dashboard as a Space
 # defaults write com.apple.dock dashboard-in-overlay -bool false
-#
-# # Use list view in all Finder windows by default
-# # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-# defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-#
-# # When performing a search, search the current folder by default
-# defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-#
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
 # # Show icons for hard drives, servers, and removable media on the desktop
 # defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 # defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
